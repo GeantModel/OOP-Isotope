5a88633b76f588e4659319ed5bc39b66c8082671
Yakov <jeykeyel19@gmail.com>
2017-12-14 20:03:09 +0300

    qq

--- Element.h
+++ Element.h
@@ -2,8 +2,6 @@
 #define UNTITLED1_ELEMENT_H
 #include <vector>
 class Element {
-private: int *arr;
-    int numb;
 public: vector<isotope> mas;
      void setmas(const vector <isotope> &mas) {
         Element::mas = mas;
@@ -14,7 +12,25 @@ public: vector<isotope> mas;
                 <<it.getN()<<", a = "<<it.getA()<<"*g/mole, fm = "<<it.getFm()
                 <<"  cs-(N,TOT) = "<<it.getCs()<<endl;
         }
+        cout<<endl;
     };
-
-};
+    void maxCS(){
+        double k=0;
+        int n=0;
+        for (int i=0; i<=mas.size(); i++){
+            if (mas[i].getCs()>k) {
+                k=mas[i].getCs();
+                n=i; } }
+        cout<<"Max cross section = "<<k<<"---Isotope "<<mas[n].getName()<<endl;
+        }
+    void maxFM(){
+        double k=0;
+        int n=0;
+        for (int i=0; i<=mas.size(); i++){
+            if (mas[i].getFm()>k) {
+                k=mas[i].getFm();
+                n=i; } }
+        cout<<"Max fm = "<<k<<"---Isotope "<<mas[n].getName()<<endl;
+    }
+  };
 #endif
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 int main() {
-    isotope*C1 = new isotope("Fe57", 26, 56, 55.9, 91.754,223.688 );
+    isotope*C1 = new isotope("Fe56", 26, 56, 55.9, 91.754,223.688);
     isotope*C2 = new isotope("Fe57", 26, 57, 56.9, 2.119, 129.388);
     isotope*C3 = new isotope("Fe58", 26, 58, 57.9, 0.282, 122.487);
     vector<isotope> a;
@@ -16,4 +16,6 @@ int main() {
     Element*B = new Element;
     B->setmas(a);
     B->getinfo();
+    B->maxCS();
+    B->maxFM();
 }
